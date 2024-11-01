c43bb356cd4733b496c45c7a6f330451bf0a6b8e
lotusCupcake <fikriansari.mfa@gmail.com>
2024-11-01 19:23:23 +0700

    add: author module

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const testRoutes = require("./test_routes");
 const categoryRouter = require("./category_routes");
 const bookRouter = require("./book_routes");
+const authorRouter = require("./author_routes");
 
 const routes = express.Router();
 
 routes.use(testRoutes);
 routes.use(categoryRouter);
 routes.use(bookRouter);
+routes.use(authorRouter);
 
 module.exports = routes;
